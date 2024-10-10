5176d13b4182f2a0f25be1376f8511163c9c4842
Thiviya <ut01208tic2024@gmail.com>
2024-10-10 17:19:49 +0900

    Add project files.

---
+++ MovieRentalmanagement/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalmanagement
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public decimal RentalPrice { get; set; }
+
+        public Movie(int movieId, string title, string director, decimal rentalPrice)
+        {
+            MovieId = movieId;
+            Title = title;
+            Director = director;
+            RentalPrice = rentalPrice;
+        }
+
+        public override string ToString()
+        {
+            return $" ID: {MovieId}, Title: {Title}, Director: {Director}, RentalPrice: {RentalPrice}";
+        }
+    }
+}
---
+++ MovieRentalmanagement/MovieManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalmanagement
+{
+    public class MovieManager
+    {
+        public List<Movie> MovieList = new List<Movie>();
+
+        public void createMovie(int movieId, string title, string director, decimal rentalPrice)
+        {
+            Movie movie = new Movie(movieId, title, director, rentalPrice); 
+
+        }
+    }
+}
